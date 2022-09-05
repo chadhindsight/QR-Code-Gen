dc333defb09db94e77cf271fad4978675c406d47
chad hinds <chadrickj8@gmail.com>
2022-09-05 11:12:54 -0400

    Junior Full Stack Engineer

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ const onGenerateSubmit = (e) => {
             setTimeout(() => {
                 // Get save url
                 const saveUrl = qr.querySelector('img').src;
-                // Create save button
+                // Create a save button
                 createSaveBtn(saveUrl);
             }, 50);
         }, 1000);
